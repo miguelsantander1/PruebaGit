f88c70048d4dbe76ef6defa2b178254fbe0de9c5
miguelsantander1 <msantander138@gmail.com>
2026-05-07 13:30:37 -0300

    segundoCommit

---
+++ pruebaGit/src/test/java/com/prueba/pruebaGit/PruebaGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.pruebaGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
